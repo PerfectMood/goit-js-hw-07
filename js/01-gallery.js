6e39e7e2e0a725da3f3e6b9c491f3164acc105f8
Bohdan Frentser <bohdan.frentser@gmail.com>
2021-12-15 00:50:33 +0200

    update

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,34 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const instance = basicLightbox.create(`
+	<h1>Dynamic Content</h1>
+	<p>You can set the content of the lightbox with JS.</p>
+  <img src="" />
+`);
 
-console.log(galleryItems);
+const createGalleryItemMarkup = ({ preview, description, original }) => {
+  return `<li class="gallery__item">
+  <a
+    class="gallery__link"
+    href = '${original}'
+  >
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+  </li>`;
+};
+const galleryList = document.querySelector('.gallery');
+const galleryMarkup = galleryItems.map(createGalleryItemMarkup).join('');
+galleryList.insertAdjacentHTML('afterbegin', galleryMarkup);
+
+console.log(galleryMarkup);
+const galleryItem = document.querySelector('gallery');
+galleryItem.addEventListener('click', ongalleryItemClick);
+
+function ongalleryItemClick() {
+  instance.show();
+}
