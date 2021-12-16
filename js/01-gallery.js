421d83ae1ee1a40d8519dbef8e5ca9fa37455313
Bohdan Frentser <bohdan.frentser@gmail.com>
2021-12-16 20:08:50 +0200

    update 02 lightbox

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,38 +1,42 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
-const instance = basicLightbox.create(`
-	<h1>Dynamic Content</h1>
-	<p>You can set the content of the lightbox with JS.</p>
-  <img src="" />
-`);
 
 const createGalleryItemMarkup = ({ preview, description, original }) => {
-  return `<li class="gallery__item">
-  <a
-    class="gallery__link"
-    href = '${original}'
-  >
+  return `<div class="gallery__item">
+  <a class="gallery__link" href=${original}>
     <img
       class="gallery__image"
-      src="${preview}"
-      data-source="${original}"
-      alt="${description}"
+      src=${preview}
+      data-source=${original}
+      alt=${description}
     />
   </a>
-  </li>`;
+</div>
+`;
 };
+
 const galleryList = document.querySelector('.gallery');
 const galleryMarkup = galleryItems.map(createGalleryItemMarkup).join('');
 galleryList.insertAdjacentHTML('afterbegin', galleryMarkup);
 
 console.log(galleryMarkup);
-const galleryItem = document.querySelector('.gallery');
-galleryItem.addEventListener('click', ongalleryItemClick);
+galleryList.addEventListener('click', ongalleryItemClick);
 
 function ongalleryItemClick(evt) {
-  if (!evt.target.classList.contains('gallery__item')) {
+  evt.preventDefault();
+  if (evt.target.classList.contains('gallery__item')) {
     return;
   }
-  evt.preventDefault();
+  const instance = basicLightbox.create(`
+  <div class="modal">
+      <img
+    src=${evt.target.dataset.source}
+    alt=${evt.target.alt}
+  />
+  </div>
+`);
+
   instance.show();
 }
+
+// AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA!!!!!!!!!!!!!!!!!!!!!
